63a3035a849258ea038d0c995de982039533e9fb
nchh12 <hoanghuync12@gmail.com>
2020-12-03 23:34:46 +0700

    Draw game title, build load/save menu

--- game.cpp
+++ game.cpp
@@ -1,4 +1,3 @@
-#pragma once
 #include "game.h"
 
 Game::Game() : Console()
@@ -10,6 +9,9 @@ void Game::Init()
 {
     //for (int i = 0; i < 7; i++)
     //  strcpy(screen + (25 + i) * CONSOLE_WIDTH + 53, (char *&)WELCOME[i]);
+
+	//draw game name at the center
+    UpdateScreen(33, 5, GAMENAME);
 
     //draw welcome at the center
     UpdateScreen(53, 25, WELCOME);
@@ -34,6 +36,37 @@ void Game::InitString()
     WELCOME[4] = "                3.  Settings";
     WELCOME[5] = "                0.    Exit";
     WELCOME[6] = "               ---------------";
+
+    GAMENAME.assign(11, "");
+    GAMENAME[0] = "      _|_|_|_|  _|_|_|_|  _|_|_|_|  _|_|_|_|  _|_|_|_|  _|  _|      _|  _|_|_|_|";
+    GAMENAME[1] = "      _|        _|    _|  _|    _|  _|        _|        _|  _|_|    _|  _|      ";
+    GAMENAME[2] = "      _|        _|_|_|_|  _|    _|  _|_|_|_|  _|_|_|_|  _|  _|  _|  _|  _|  _|_|";
+    GAMENAME[3] = "      _|        _|  _|    _|    _|        _|        _|  _|  _|    _|_|  _|    _|";
+    GAMENAME[4] = "      _|_|_|_|  _|    _|  _|_|_|_|  _|_|_|_|  _|_|_|_|  _|  _|      _|  _|_|_|_|";
+    GAMENAME[5] = "";
+    GAMENAME[6] = "                        _|_|_|_|  _|_|_|_|  _|_|_|_|  _|_|_|_\\";
+    GAMENAME[7] = "                        _|    _|  _|    _|  _|    _|  _|    _|";
+    GAMENAME[8] = "                        _|_|_|_|  _|    _|  _|_|_|_|  _|    _|";
+    GAMENAME[9] = "                        _|  _|    _|    _|  _|    _|  _|    _|";
+   GAMENAME[10] = "                        _|    _|  _|_|_|_|  _|    _|  _|_|_|_/";
+
+   LOADMENU.assign(7, "");
+   LOADMENU[0] = "_|        _|_|_|_|  _|_|_|_|  _|_|_|_\\";
+   LOADMENU[1] = "_|        _|    _|  _|    _|  _|    _|";
+   LOADMENU[2] = "_|        _|    _|  _|_|_|_|  _|    _|  _|     _|  _|_|_|_|  _|      _|  _|    _|";
+   LOADMENU[3] = "_|        _|    _|  _|    _|  _|    _|  _|_| _|_|  _|        _|_|    _|  _|    _|";
+   LOADMENU[4] = "_|_|_|_|  _|_|_|_|  _|    _|  _|_|_|_/  _|  _| _|  _|_|_|_|  _|  _|  _|  _|    _|";
+   LOADMENU[5] = "                                        _|     _|  _|        _|    _|_|  _|    _|";
+   LOADMENU[6] = "                                        _|     _|  _|_|_|_|  _|      _|  _|_|_|_|";
+   
+   SAVEMENU.assign(7, "");
+   SAVEMENU[0] = "_|_|_|_|  _|_|_|_|  _|        _|  _|_|_|_| ";
+   SAVEMENU[1] = " _|       _|    _|   _|      _|   _|";      
+   SAVEMENU[2] = "   _|     _|_|_|_|    _|    _|    _|_|_|_|  _|     _|  _|_|_|_|  _|      _|  _|    _|";
+   SAVEMENU[3] = "     _|   _|    _|     _|  _|     _|        _|_| _|_|  _|        _|_|    _|  _|    _|";
+   SAVEMENU[4] = "_|_|_|_|  _|    _|      _|_|      _|_|_|_|  _|  _| _|  _|_|_|_|  _|  _|  _|  _|    _|";
+   SAVEMENU[5] = "                                            _|     _|  _|        _|    _|_|  _|    _|";
+   SAVEMENU[6] = "                                            _|     _|  _|_|_|_|  _|      _|  _|_|_|_|";
 }
 
 void Game::WelcomeScreen()
@@ -42,4 +75,149 @@ void Game::WelcomeScreen()
         exit(0);
     if (!started && (GetAsyncKeyState((unsigned short)'1') & 0x8000))
         started = 1, CleanScreen();
+	if (!started && (GetAsyncKeyState((unsigned short)'2') & 0x8000))
+        if(!loadGameMenu())
+			Init();
+}
+
+std::vector<std::string> Game::getFilename(const std::string& name)
+{
+	std::vector<std::string> v;
+	std::string pattern(name);
+	pattern.append("\\*");
+	std::wstring stemp = std::wstring(pattern.begin(), pattern.end());
+	LPCWSTR sw = stemp.c_str();
+	WIN32_FIND_DATAW data;
+	HANDLE hFind;
+	if ((hFind = FindFirstFileW(sw, &data)) != INVALID_HANDLE_VALUE) {
+		do {
+			wchar_t* txt = data.cFileName;
+			std::wstring ws(txt);
+			// your new String
+			std::string str(ws.begin(), ws.end());
+			if (str[0] == '.') continue;
+			// Show String
+			v.push_back(str);
+			//Just test !
+			//cout << str << endl;
+		} while (FindNextFileW(hFind, &data) != 0);
+		FindClose(hFind);
+	}
+	return v;
+}
+
+bool Game::loadGameMenu()
+{
+	std::string filename;
+	CleanScreen();
+	std::vector <std::string> files = getFilename("data");
+	if (files.size() == 0) {
+		UpdateScreen(65, 30, "NO SAVED FILES EXIST!");
+		return false;
+	}
+	int curPos = 0;
+	CleanScreen();
+
+	UpdateScreen(38, 5, LOADMENU);
+	UpdateScreen(36, 13, "<Press ESC to escape...>");
+	UpdateScreen(36, 15, "Choose Filename to load: ");
+
+	
+
+	for (int i = 0; i < (int)files.size(); ++i) {
+		if (i == curPos) {
+			UpdateScreen(26, 16 + i, ">> ");
+		}
+		UpdateScreen(30, 16 + i, files[i]);
+	}
+	OutScreen();
+	while (true) {
+		if ((GetAsyncKeyState((unsigned short)'W') & 0x8000))
+		{
+			UpdateScreen(26, 16 + curPos, "    " + files[curPos]);
+			curPos--;
+			curPos = (curPos + files.size()) % files.size();
+			UpdateScreen(26, 16 + curPos, ">>  " + files[curPos]);
+			OutScreen();
+		}
+		if ((GetAsyncKeyState((unsigned short)'S') & 0x8000))
+		{
+			UpdateScreen(26, 16 + curPos, "    " + files[curPos]);
+			curPos++;
+			curPos = (curPos + files.size()) % files.size();
+			UpdateScreen(26, 16 + curPos, ">>  " + files[curPos]);
+			OutScreen();
+		}
+		if ((GetAsyncKeyState((unsigned short)VK_RETURN) & 0x8000)) //enter
+		{
+			isLoaded = true;
+			loadGame(files[curPos]);
+			CleanScreen();
+			return true;
+		}
+		if ((GetAsyncKeyState((unsigned short)VK_ESCAPE) & 0x8000)) //esc
+		{
+			CleanScreen();
+			return false;
+		}
+	}
+	Sleep(200);
+}
+
+bool Game::loadGame(std::string file) {
+	std::ifstream infile("./data/" + file);
+	if (!infile.is_open()) {
+		return false;
+	}
+
+	int playerX, playerY;
+	infile >> playerX; //load player coordinate
+	infile >> playerY;
+	//Player.~CPEOPLE();
+	//new(&Player) CPEOPLE((playerX, playerY));
+
+	infile.close();
+	return true;
+}
+
+void Game::saveGameMenu() { // get file of cMap ma
+	std::string filename;
+	CleanScreen();
+	
+	UpdateScreen(15, 5, SAVEMENU);
+	UpdateScreen(15, 20, "<Press ESC to escape...>");
+
+	UpdateScreen(15, 15, "Input file name to save: ");
+
+	char key;
+	while ((key = _getch()) != 27) {
+		switch (key) {
+		case '\b':
+			if (filename.size() != 0) {
+				filename.pop_back();
+				UpdateScreen(50, 15, "                                                 " + filename);
+				OutScreen();
+			}
+			break;
+		case 13:
+			saveGame(filename);
+			break;
+		default:
+			filename.push_back(key);
+			UpdateScreen(50, 15, filename);
+			OutScreen();
+		}
+		if (key == 13) break;
+	}
+	CleanScreen();
+	OutScreen();
+}
+
+void Game::saveGame(std::string file)
+{
+	std::ofstream outfile("./data/" + file + ".txt");
+	//outfile << Player.mX;
+	//outfile << Player.mY;
+
+	outfile.close();
 }
--- game.h
+++ game.h
@@ -1,12 +1,17 @@
 #pragma once
-#include "console.cpp"
+#include "console.h"
+#include <fstream>
+#include <conio.h>
 
 class Game : public Console
 {
 private:
     std::vector<std::string> WELCOME;
+    std::vector<std::string> GAMENAME;
+    std::vector<std::string> LOADMENU;
+    std::vector<std::string> SAVEMENU;
     void InitString();
-
+    bool isLoaded = false;
 public:
     int started = 0;
     Game();
@@ -15,5 +20,10 @@ public:
     void Pause();
     void Crash();
     void WelcomeScreen();
+    bool loadGameMenu();
+    std::vector<std::string> getFilename(const std::string& name);
+    bool loadGame(std::string file);
+    void saveGameMenu();
+    void saveGame(std::string file);
     ~Game();
 };
