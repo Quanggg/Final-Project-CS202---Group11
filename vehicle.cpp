2542e89f6f038e63385fe242186998e4bad55eb7
Thang <63545195+group13-project@users.noreply.github.com>
2021-01-08 19:07:22 +0700

    create car with random distance

--- vehicle.cpp
+++ vehicle.cpp
@@ -83,6 +83,27 @@ Vehicle::~Vehicle()
 }
 
 //
+void Vehicle::CreateObj()
+{
+    srand(time(NULL));
+    Object* v;
+    int _RandomDistance, NEW_LOCATION;
+    //create 4 car obj with random distance
+    //car1 car2 car3 car4
+    for (int i = 0; i < 3; i++)
+    {
+        if (!i)
+            v = new Car(MAP._x + 2, MAP._y + CAR_LOCATION_Y);
+        else
+        {
+            _RandomDistance = rand() % 25 + 1;
+            NEW_LOCATION = _CarLane[i - 1]->_Pos._x + CAR_LENGTH + _RandomDistance;
+
+            v = new Car(NEW_LOCATION, MAP._y + CAR_LOCATION_Y);
+        }
+        _CarLane.push_back(v);
+    }
+}
 void Vehicle::Move(const int& ElapsedTime)
 {
     Vehicle::CarMove(ElapsedTime);
